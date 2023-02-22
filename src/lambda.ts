d7d0f810b0ea4e6869763155fbc0a66756dd832a
Meng Jun <813935532@qq.com>
2023-02-22 16:47:51 +0800

    feat: function name

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import serverless from '@vendia/serverless-express';
 
+console.log(serverless);
+
 let server;
 
 async function bootstrap() {
